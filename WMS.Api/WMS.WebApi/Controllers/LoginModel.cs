e7f8ef5f52c81b8c234cdd8457b828c590838895
Patricia Atieyeh <patricia.atieyeh@gmail.com>
2018-05-01 16:49:41 +0300

    Added "RegistrationModel" class

--- WMS.Api/WMS.WebApi/Controllers/LoginModel.cs
+++ WMS.Api/WMS.WebApi/Controllers/LoginModel.cs
@@ -6,4 +6,17 @@
 
         public string Password { get; set; }
     }
+
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string TelephoneNumber { get; set; }
+
+        public string Address { get; set; }
+    }
 }
