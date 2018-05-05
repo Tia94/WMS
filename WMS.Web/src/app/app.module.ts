694ff648efc23248878abcedfe9d5f3636516125
Patricia Atieyeh <patricia.atieyeh@gmail.com>
2018-05-05 11:02:01 +0300

    Added anglar components for login and registration

--- WMS.Web/src/app/app.module.ts
+++ WMS.Web/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterClientComponent } from './register-client/register-client.component';
+
+// const appRoutes: Routes = [
+//   { path: 'crisis-center', component: CrisisListComponent },
+//   { path: 'hero/:id',      component: HeroDetailComponent },
+//   {
+//     path: 'heroes',
+//     component: HeroListComponent,
+//     data: { title: 'Heroes List' }
+//   },
+//   { path: '',
+//     redirectTo: '/heroes',
+//     pathMatch: 'full'
+//   },
+//   { path: '**', component: PageNotFoundComponent }
+// ];
+
+const appRoutes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterClientComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterClientComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{ enableTracing: true }) // <-- debugging purposes only
   ],
   providers: [],
   bootstrap: [AppComponent]
