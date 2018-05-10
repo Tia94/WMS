8f7c6aa22874d4e054f5deba0ccc27b9118596e8
Sp1n89 <rami.atieyeh@gmail.com>
2018-05-10 21:27:44 +0300

    Small fix

--- WMS.Api/WMS.Infrastructure.Repository/DbInitializer.cs
+++ WMS.Api/WMS.Infrastructure.Repository/DbInitializer.cs
@@ -12,15 +12,51 @@ namespace WMS.Infrastructure.Repository
             // Look for any students.
             if (context.Users.Any())
             {
-                return;   // DB has been seeded
+                return; // DB has been seeded
             }
 
-            var users = new User[]
+            var users = new[]
             {
-                new User{Username="admin",Firstname="Carson",Lastname="Alexander",Password="1234",Email="admin@gmail.com",TelephoneNumber="0933594236",Role=Role.Admin},
-                new User{Username="client",Firstname="Patricia",Lastname="Atieyeh",Password="1234",Email="patricia.atieyeh@gmail.com",TelephoneNumber="0933066835",Role=Role.Client},
-                new User{Username="driver",Firstname="Said",Lastname="Atieyeh",Password="1234",Email="driver@gmail.com",TelephoneNumber="0933066835",Role=Role.Driver},
-                new User{Username="keeper",Firstname="Rami",Lastname="Atieyeh",Password="1234",Email="keeper@gmail.com",TelephoneNumber="0933066835",Role=Role.Keeper},
+                new User
+                {
+                    Username = "admin",
+                    Firstname = "System",
+                    Lastname = "Admin",
+                    Password = "1234",
+                    Email = "admin@gmail.com",
+                    TelephoneNumber = "0933594236",
+                    Role = Role.Admin
+                },
+                new User
+                {
+                    Username = "client",
+                    Firstname = "App",
+                    Lastname = "Client",
+                    Password = "1234",
+                    Email = "client@gmail.com",
+                    TelephoneNumber = "0933066835",
+                    Role = Role.Client
+                },
+                new User
+                {
+                    Username = "driver",
+                    Firstname = "Truck",
+                    Lastname = "Driver",
+                    Password = "1234",
+                    Email = "driver@gmail.com",
+                    TelephoneNumber = "0933066835",
+                    Role = Role.Driver
+                },
+                new User
+                {
+                    Username = "keeper",
+                    Firstname = "Store",
+                    Lastname = "Keeper",
+                    Password = "1234",
+                    Email = "keeper@gmail.com",
+                    TelephoneNumber = "0933066835",
+                    Role = Role.Keeper
+                },
             };
 
             context.Users.AddRange(users);
