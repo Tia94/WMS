6408071d7dcf8bf2ee79754b1dc76423cf546956
Patricia Atieyeh <patricia.atieyeh@gmail.com>
2018-05-22 20:06:10 +0300

    Register form added.

--- WMS.Web/src/app/core/register/register.component.ts
+++ WMS.Web/src/app/core/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public registerForm: FormGroup;
+  public username: FormControl;
+  public password: FormControl;
+  public firstName: FormControl;
+  public lastName: FormControl;
+  public email: FormControl;
+  public telephoneNumber: FormControl;
+  public address: FormControl;
 
-  ngOnInit() {
+  private role;
+
+  constructor() { 
+
   }
 
+  ngOnInit() : void {
+    this.createFormControls();
+    this.createForm();
+  }
+
+  private createFormControls() {
+    this.username = new FormControl("", Validators.required);
+    this.password = new FormControl("", Validators.required);
+    this.firstName = new FormControl("", Validators.required);
+    this.lastName = new FormControl("", Validators.required);
+    this.lastName = new FormControl("", Validators.required);
+    this.address = new FormControl("", Validators.required);
+  }
+
+  private createForm() {
+    this.registerForm = new FormGroup({
+      username: this.username,
+      password: this.password,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      address: this.address,
+      telephoneNumber: this.telephoneNumber,
+      email: this.email
+    });
+
+}
 }
