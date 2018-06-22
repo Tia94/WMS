386aaaad39c5934d5ec4d7501cee54c8745583a2
Patricia Atieyeh <patricia.atieyeh@gmail.com>
2018-06-22 11:14:52 +0300

    Added products and order classes and a Product controller

---
+++ WMS.Api/WMS.Domain.Model/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WMS.Domain.Model
+{
+    class Order
+    {
+        List<Product> products { get; set; }
+        User user { get; set; }
+        public decimal totalPrice { get; set; }
+
+        public Order()
+        {
+            foreach (Product product in products)
+            {
+                this.totalPrice = 0;
+                this.totalPrice += product.Price;
+            }
+        }
+    }
+}
