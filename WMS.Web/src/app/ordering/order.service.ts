b88ab1f125ffe232831d58cf2ad69c9ceef010ad
Sp1n89 <rami.atieyeh@gmail.com>
2018-07-17 15:36:06 +0300

    Ordering - WIP

--- WMS.Web/src/app/ordering/order.service.ts
+++ WMS.Web/src/app/ordering/order.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Injectable()
 export class OrderService {
@@ -20,64 +19,64 @@ export class OrderService {
   public addToCart(username: string, productId: number, quantity: number): void {
     debugger;
 
-    let key = this.orderKey(username);
-    let orderJSON = localStorage.getItem(key);
-    let order: Order;
-    if (orderJSON) {
-      order = Order.FromJSON(orderJSON);
-      order.addItem(productId, quantity);
+    let key = this.getCartKey(username);
+    let cartJSON = localStorage.getItem(key);
+    let cart: Cart;
+    if (cartJSON) {
+      cart = Cart.FromJSON(cartJSON);
+      cart.addItem(productId, quantity);
     }
     else {
-      order = new Order(username);
-      order.addItem(productId, quantity);
+      cart = new Cart(username);
+      cart.addItem(productId, quantity);
     }
 
-    localStorage.setItem(key, JSON.stringify(order));
+    localStorage.setItem(key, JSON.stringify(cart));
   }
 
   public removeFromCart(username: string, productId: number): void {
-    let key = this.orderKey(username);
-    let orderJSON = localStorage.getItem(key);
+    let key = this.getCartKey(username);
+    let cartJSON = localStorage.getItem(key);
     debugger;
-    let order: Order = Order.FromJSON(orderJSON);
-    order.removeItem(productId);
-    localStorage.setItem(key, JSON.stringify(order));
+    let cart: Cart = Cart.FromJSON(cartJSON);
+    cart.removeItem(productId);
+    localStorage.setItem(key, JSON.stringify(cart));
   }
 
-  private orderKey(username: string): string {
-    return `${username}_order`;
+  private getCartKey(username: string): string {
+    return `${username}_cart`;
   }
 
 }
 
-class Order {
+class Cart {
 
-  private _items: Array<OrderItem>;
-  public get items(): Array<OrderItem> {
+  private _items: Array<CartItem>;
+  public get items(): Array<CartItem> {
     return this._items;
   }
 
   constructor(public username: string) {
-    this._items = new Array<OrderItem>();
+    this._items = new Array<CartItem>();
   }
 
-  public static FromJSON(orderJSON: string): Order {
-    if (orderJSON === null || orderJSON === undefined)
-      throw new Error("orderJSON can not be null or undefined");
+  public static FromJSON(cartJSON: string): Cart {
+    if (cartJSON === null || cartJSON === undefined)
+      throw new Error("cartJSON can not be null or undefined");
 
-    let storageOrder = JSON.parse(orderJSON);
+    let cart = JSON.parse(cartJSON);
 
-    let order2 = new Order(storageOrder.username);
-    storageOrder._items.forEach(item => {
-      order2.addItem(item.productId, item.quantity);
+    let updatedCart = new Cart(cart.username);
+    cart._items.forEach(item => {
+      updatedCart.addItem(item.productId, item.quantity);
     });
-    return order2;
+    return updatedCart;
   }
 
   public addItem(productId: number, quantity: number): void {
     debugger;
     if (!this.items.some(x => x.productId === productId)) {
-      this.items.push(new OrderItem(productId, quantity));
+      this.items.push(new CartItem(productId, quantity));
     }
   }
 
@@ -100,7 +99,7 @@ class Order {
 
 }
 
-class OrderItem {
+class CartItem {
   constructor(public productId: number, public quantity: number) {
 
   }
